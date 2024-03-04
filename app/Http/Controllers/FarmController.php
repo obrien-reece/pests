567e519776ad24a6d694eac92bd03e18a93275e1
obrien-reece <indeche.evans.o@gmail.com>
2024-03-04 22:33:57 +0300

    finished farmer saving preliminary data after login

--- app/Http/Controllers/FarmController.php
+++ app/Http/Controllers/FarmController.php
@@ -77,61 +77,70 @@ class FarmController extends Controller
     }
 
     public function confirmfarmersanswers(Request $request){
-      if($request->isDirty('farm_name') || $request->isDirty('farm_location') || $request->isDirty('crop_name') || $request->isDirty('production_years') || $request->isDirty('farm_size') || $request->isDirty('farming_type') || $request->isDirty('pest') || $request->isDirty('pest_frequency')) {
-        Farm::where('farmer_id', Auth::id())->update([
-          'farm_size' => $request->input('farm_size'),
-          'farm_name' => $request->input('farm_name'),
-          'farm_location' => $request->input('farm_location'),
-          'crop_name' => $request->input('crop_name'),
-          'pest_frequency' => $request->input('pest_frequency'),
-          'pest' => $request->input('pest'),
-          'crop_names' => $request->input('crop_names'),
-          'production_years' => $request->input('production_years'),
-          'farming_type' => $request->input('farming_type'),
-        ]);
-      }
+        $originalFarm = Farm::where('farmer_id', Auth::id())->first();
 
-      return redirect()->route('/');
+        $updatedFarm = [
+            'farm_size' => $request->input('farm_size'),
+            'farm_name' => $request->input('farm_name'),
+            'farm_location' => $request->input('farm_location'),
+            'crop_name' => $request->input('crop_name'),
+            'pest_frequency' => $request->input('pest_frequency'),
+            'pest' => $request->input('pest'),
+            'crop_names' => $request->input('crop_names'),
+            'production_years' => $request->input('production_years'),
+            'farming_type' => $request->input('farming_type'),
+        ];
+
+        foreach ($updatedFarm as $key => $value) {
+            if ($originalFarm->$key != $value) {
+                $originalFarm->$key = $value;
+            }
+        }
+
+        if ($originalFarm->isDirty()) {
+            $originalFarm->save();
+        }
+
+        return redirect()->route('dashboard-analytics');
     }
 
+    /**
+       * Store a newly created resource in storage.
+       */
+    public function store(Request $request)
+    {
+      //
+    }
 
-  /**
-     * Store a newly created resource in storage.
-     */
-  public function store(Request $request)
-  {
-    //
-  }
+    /**
+       * Display the specified resource.
+       */
+    public function show(Farm $farmersCrops)
+    {
+      //
+    }
 
-  /**
-     * Display the specified resource.
-     */
-  public function show(Farm $farmersCrops)
-  {
-    //
-  }
+    /**
+       * Show the form for editing the specified resource.
+       */
+    public function edit(Farm $farmersCrops)
+    {
+      //
+    }
 
-  /**
-     * Show the form for editing the specified resource.
-     */
-  public function edit(Farm $farmersCrops)
-  {
-    //
-  }
+    /**
+       * Update the specified resource in storage.
+       */
+    public function update(Request $request, Farm $farmersCrops)
+    {
+      //
+    }
 
-  /**
-     * Update the specified resource in storage.
-     */
-  public function update(Request $request, Farm $farmersCrops)
-  {
-    //
-  }
-
-  /**
-     * Remove the specified resource from storage.
-     */
-  public function destroy(Farm $farmersCrops)
-  {
-    //
-  }
+    /**
+       * Remove the specified resource from storage.
+       */
+    public function destroy(Farm $farmersCrops)
+    {
+      //
+    }
 }
