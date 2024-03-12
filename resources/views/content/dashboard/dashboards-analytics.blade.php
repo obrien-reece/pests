e1f7aa74a701ef093a248d74d5ab613ad47774bd
obrien-reece <indeche.evans.o@gmail.com>
2024-03-12 13:25:08 +0300

    so far so good

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -137,97 +137,8 @@
   <!-- Farmer location area weather -->
 
   <div class="col-xl-4 col-md-6">
-    <div class="card">
-      <div class="card-header d-flex align-items-center justify-content-between">
-        <h5 class="card-title m-0 me-2">Total Earning</h5>
-        <div class="dropdown">
-          <button class="btn p-0" type="button" id="totalEarnings" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="mdi mdi-dots-vertical mdi-24px"></i>
-          </button>
-          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="totalEarnings">
-            <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
-            <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
-            <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
-          </div>
-        </div>
-      </div>
-      <div class="card-body">
-        <div class="mb-3 mt-md-3 mb-md-5">
-          <div class="d-flex align-items-center">
-            <h2 class="mb-0">$24,895</h2>
-            <span class="text-success ms-2 fw-medium">
-              <i class="mdi mdi-menu-up mdi-24px"></i>
-              <small>10%</small>
-            </span>
-          </div>
-          <small class="mt-1">Compared to $84,325 last year</small>
-        </div>
-        <ul class="p-0 m-0">
-          <li class="d-flex mb-4 pb-md-2">
-            <div class="avatar flex-shrink-0 me-3">
-              <img src="{{asset('assets/img/icons/misc/zipcar.png')}}" alt="zipcar" class="rounded">
-            </div>
-            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-              <div class="me-2">
-                <h6 class="mb-0">Zipcar</h6>
-                <small>Vuejs, React & HTML</small>
-              </div>
-              <div>
-                <h6 class="mb-2">$24,895.65</h6>
-                <div class="progress bg-label-primary" style="height: 4px;">
-                  <div class="progress-bar bg-primary" style="width: 75%" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex mb-4 pb-md-2">
-            <div class="avatar flex-shrink-0 me-3">
-              <img src="{{asset('assets/img/icons/misc/bitbank.png')}}" alt="bitbank" class="rounded">
-            </div>
-            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-              <div class="me-2">
-                <h6 class="mb-0">Bitbank</h6>
-                <small>Sketch, Figma & XD</small>
-              </div>
-              <div>
-                <h6 class="mb-2">$8,6500.20</h6>
-                <div class="progress bg-label-info" style="height: 4px;">
-                  <div class="progress-bar bg-info" style="width: 75%" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex mb-md-3">
-            <div class="avatar flex-shrink-0 me-3">
-              <img src="{{asset('assets/img/icons/misc/aviato.png')}}" alt="aviato" class="rounded">
-            </div>
-            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-              <div class="me-2">
-                <h6 class="mb-0">Aviato</h6>
-                <small>HTML & Angular</small>
-              </div>
-              <div>
-                <h6 class="mb-2">$1,2450.80</h6>
-                <div class="progress bg-label-secondary" style="height: 4px;">
-                  <div class="progress-bar bg-secondary" style="width: 75%" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-
-  <!-- Four Cards -->
-  <div class="col-xl-4 col-md-6">
     <div class="mb-4 d-flex justify-content-between align-items-center">
       <h5 class="fw-bold mb-0">List of Pests in {{ $farmerdetails->farm_location }}</h5>
-      <div>
-        <a href="{{ route('index-of-pests', ['farmer' => Auth::id(), 'location' => $farmerdetails->farm_location]) }}">
-          <i class="mdi mdi-eye-outline" title="View All"></i>
-        </a>
-      </div>
     </div>
     <div class="row gy-4">
       @foreach($pests as $pest)
@@ -257,6 +168,46 @@
       @endif
       @endforeach
   </div>
+</div>
+
+<!-- Four Cards -->
+<div class="col-xl-4 col-md-6">
+  <div class="mb-4 d-flex justify-content-between align-items-center">
+    <h5 class="fw-bold mb-0"></h5>
+    <div>
+      <a href="{{ route('index-of-pests', ['farmer' => Auth::id(), 'location' => $farmerdetails->farm_location]) }}">
+        <i class="mdi mdi-eye-outline" title="View All"></i>
+      </a>
+    </div>
+  </div>
+  <div class="row gy-4">
+    @foreach($pests as $pest)
+    @if($loop->index > 4 && $loop->index < 9)
+    <div class="col-sm-6">
+      <div class="card h-100">
+        <div class="card-header d-flex align-items-center justify-content-between">
+          <div class="avatar">
+            <div class="avatar-initial bg-danger rounded-circle shadow-sm">
+              <i class="mdi mdi-bug-outline"></i>
+            </div>
+          </div>
+        </div>
+        <div class="card-body mt-mg-1">
+          <h6 class="mb-2">{{ $pest->pest_name }}</h6>
+          <div class="d-flex flex-wrap align-items-center mb-2 pb-1">
+            <h4 class="mb-0 me-2"><small style="font-size: 12px;">{{ $pest->crop_attacked }}</small></h4>
+            <small class="text-danger mt-1">{{ $pest->devastation_severity }}</small>
+          </div>
+          <a href="{{ route('show-pest', ['id' => $pest->id]) }}" style="color: grey; font-size: 12px;">
+            More info
+            <span class="mdi mdi-chevron-right"></span>
+          </a>
+        </div>
+      </div>
+    </div>
+    @endif
+    @endforeach
+</div>
 </div>
 
 
